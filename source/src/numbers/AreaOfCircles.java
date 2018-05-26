2a2d06091cfb95ad095d7419b84e20359642884d
Shreenivash Sharma <shreenivash.sharma@timesinternet.in>
2018-05-26 14:37:04 +0530

    Area of circles

---
+++ source/src/numbers/AreaOfCircles.java
@@ -0,0 +1,21 @@
+package src.numbers;
+
+import java.util.Scanner;
+
+public class AreaOfCircles {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the Radius");
+		double radius;
+		double area;
+		
+		radius= sc.nextInt();
+		area= radius*radius* Math.PI;
+		System.out.println(area);
+	
+	}
+	
+
+}
