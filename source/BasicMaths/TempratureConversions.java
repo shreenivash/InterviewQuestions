dcafd59fc70fa4b2fd4d2480165652654ccc7865
Shreenivash Sharma <shreenivash@outlook.com>
2018-05-26 15:19:31 +0530

    Complete program

--- source/BasicMaths/TempratureConversions.java
+++ source/BasicMaths/TempratureConversions.java
@@ -1,8 +1,17 @@
 package BasicMaths;
 
+import java.util.Scanner;
+
 public class TempratureConversions {
 
 	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Which temprature conversion you want");
+		System.out.println("Enter Fahrenheit");
+		double fh= sc.nextDouble();
+		double celcius;
+		celcius= (fh-32)*(5.0/9);
+		System.out.println(celcius);
 		
 
 	}
