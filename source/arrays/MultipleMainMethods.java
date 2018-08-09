aa7e4cbfd52dfff68e97ba33e205d358ce9182f6
Shreenivash Sharma <shreenivash@outlook.com>
2018-08-09 14:56:35 +0530

    Write a java program to print numbers from 1 to 10 using 2 threads. Thread 1 should print odd and thread 2 should print Even numbers.

---
+++ source/arrays/MultipleMainMethods.java
@@ -0,0 +1,31 @@
+package arrays;
+
+
+public class MultipleMainMethods {
+
+    public static void main(String[] args, String[] args1){
+
+
+    }
+    public static void main(String[] args,String[] args1, String[] args2){
+
+
+    }
+//    public static void main(String[] args){
+//
+//
+//    }
+//    public static void main(String[] args){
+//
+//
+//    }
+//    public static void main(String[] args){
+//
+//
+//    }
+//    public static void main(String[] args){
+//
+//
+//    }
+
+}
